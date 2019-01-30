7f679b5216c3f5a30025af7269d66403e7e4b5d6
Bjorn Pijpops <pijpopsbjorn@hotmail.com>
2019-01-30 20:32:30 +0100

    add Lab 5B

--- WorkSpace/Week 1 (Typescript & Components)/contactApp/src/app/contact-form/contact-form.component.ts
+++ WorkSpace/Week 1 (Typescript & Components)/contactApp/src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Contact } from '../models/contact.model';
 
 @Component({
@@ -8,10 +9,16 @@ import { Contact } from '../models/contact.model';
 })
 export class ContactFormComponent implements OnInit {
   @Output() onSubmit: EventEmitter<Contact> = new EventEmitter();
-
-  constructor() { }
+  form: FormGroup;
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'name': new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      'email': new FormControl(null, [Validators.required, Validators.pattern("[a-z0-9_\.]+@[a-z0-9_\.]")]),
+      'phone': new FormControl(null, [Validators.required, Validators.minLength(9)]),
+      'isFavorite': new FormControl(false),
+      'avatar': new FormControl(null),
+    })
   }
 
   submit(form) {
